012706b6491ae75f7a142a5057f6cb0a7d87a4fa
felipemuniz09 <felipemuniz09@gmail.com>
2014-12-27 16:01:01 -0200

    Header

--- Avaliacao.Net/Avaliacao.Net.WebApplication/Default.aspx.designer.cs
+++ Avaliacao.Net/Avaliacao.Net.WebApplication/Default.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace Avaliacao.Net.WebApplication {
     
     
     public partial class _Default {
-        
-        /// <summary>
-        /// selecionarClientesBtn control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button selecionarClientesBtn;
     }
 }
